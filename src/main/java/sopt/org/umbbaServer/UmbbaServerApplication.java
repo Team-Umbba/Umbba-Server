e63ca102cdb5a64516afa9c044d589aa61229384
jun02160 <jun020216@naver.com>
2023-06-24 12:08:15 +0900

    initial commit

---
+++ src/main/java/sopt/org/umbbaServer/UmbbaServerApplication.java
@@ -0,0 +1,13 @@
+package sopt.org.umbbaServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmbbaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UmbbaServerApplication.class, args);
+	}
+
+}
