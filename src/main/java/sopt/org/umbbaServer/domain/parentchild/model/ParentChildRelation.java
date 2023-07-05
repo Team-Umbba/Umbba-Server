35525735b0652847b4a7c7cbd094d57d2cae3950
ddongseop <dlehdtjq00o@gmail.com>
2023-07-06 00:09:00 +0900

    [CORRECT] Enum의 String 형식 통일 #9

--- src/main/java/sopt/org/umbbaServer/domain/parentchild/model/ParentChildRelation.java
+++ src/main/java/sopt/org/umbbaServer/domain/parentchild/model/ParentChildRelation.java
@@ -8,8 +8,8 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public enum ParentChildRelation {
 
-    DAD_SON("아버지와 아들 관계"),
-    DAD_DAU("아버지와 딸 관계"),
+    DAD_SON("아빠와 아들 관계"),
+    DAD_DAU("아빠와 딸 관계"),
     MOM_SON("엄마와 아들 관계"),
     MOM_DAU("엄마와 딸 관계");
 
