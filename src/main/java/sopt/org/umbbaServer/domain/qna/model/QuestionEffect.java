6810d9713a977f996a843410b7a5c2de95fef05f
ddongseop <dlehdtjq00o@gmail.com>
2023-07-10 19:20:37 +0900

    [REFACTOR] response dto에 enum값 직접 사용하던 것 수정

--- src/main/java/sopt/org/umbbaServer/domain/qna/model/QuestionEffect.java
+++ src/main/java/sopt/org/umbbaServer/domain/qna/model/QuestionEffect.java
@@ -1,5 +1,15 @@
 package sopt.org.umbbaServer.domain.qna.model;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public enum QuestionEffect {
-    효용1, 효용2, 효용3
+    DREAM("꿈에 대한 생각"),
+    SYMPATHY("공감대형성"),
+    HOOKING("후킹 요소");
+
+    private final String value;
 }
