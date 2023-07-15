c5528c3e87f8940422af72451c2aa4bede31d9e1
ddongseop <dlehdtjq00o@gmail.com>
2023-07-15 18:00:27 +0900

    [FIX] request dto의 필드명이 snake_case로 받아와지도록 변경 #43

--- src/main/java/sopt/org/umbbaServer/domain/qna/controller/dto/request/TodayAnswerRequestDto.java
+++ src/main/java/sopt/org/umbbaServer/domain/qna/controller/dto/request/TodayAnswerRequestDto.java
@@ -1,5 +1,7 @@
 package sopt.org.umbbaServer.domain.qna.controller.dto.request;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,6 +10,7 @@ import javax.validation.constraints.NotBlank;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class TodayAnswerRequestDto {
 
     @NotBlank // null, "", " "을 모두 허용하지 X
--- src/main/java/sopt/org/umbbaServer/domain/user/controller/dto/request/RefreshRequestDto.java
+++ src/main/java/sopt/org/umbbaServer/domain/user/controller/dto/request/RefreshRequestDto.java
@@ -1,11 +1,14 @@
 package sopt.org.umbbaServer.domain.user.controller.dto.request;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class RefreshRequestDto {
 
     private Long userId;
