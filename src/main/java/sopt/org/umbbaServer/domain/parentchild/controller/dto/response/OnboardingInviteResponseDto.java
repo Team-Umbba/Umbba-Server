1b5252fbd7ce16626da1d6a6fe267d8ec3379dcd
ddongseop <dlehdtjq00o@gmail.com>
2023-07-15 17:04:08 +0900

    [FIX] 푸시 알림 시간이 23:00:00 형식으로 리턴되는 것 23:00으로 리턴되게 수정 #43

--- src/main/java/sopt/org/umbbaServer/domain/parentchild/controller/dto/response/OnboardingInviteResponseDto.java
+++ src/main/java/sopt/org/umbbaServer/domain/parentchild/controller/dto/response/OnboardingInviteResponseDto.java
@@ -1,5 +1,6 @@
 package sopt.org.umbbaServer.domain.parentchild.controller.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Builder;
@@ -21,6 +22,7 @@ public class OnboardingInviteResponseDto {
 
     private String parentchildRelation;
 
+    @JsonFormat(pattern = "kk:mm")
     private LocalTime pushTime;
 
     private String inviteCode;
