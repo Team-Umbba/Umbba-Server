c6c857c41fecf70b08b13f982fd225e71a0e87e1
jun02160 <jun020216@naver.com>
2023-07-16 15:03:28 +0900

    [CHORE] Security Config에 추가

--- src/main/java/sopt/org/umbbaServer/global/util/fcm/controller/FCMController.java
+++ src/main/java/sopt/org/umbbaServer/global/util/fcm/controller/FCMController.java
@@ -21,20 +21,22 @@ public class FCMController {
     private final FCMService fcmService;
     private final FCMScheduler fcmScheduler;
 
+
+    @PostMapping("/qna")
+    @ResponseStatus(HttpStatus.OK)
+    public ApiResponse sendTopicScheduledTest() {
+        return ApiResponse.success(SuccessType.PUSH_ALARM_PERIODIC_SUCCESS, fcmScheduler.pushTodayQna());
+    }
+
+
+    /**
+     * 주기적 알림 전송 테스트를 위한 API
+     */
     @PostMapping
     @ResponseStatus(HttpStatus.OK)
     public ApiResponse sendNotificationByToken(@RequestBody FCMPushRequestDto request, Principal principal) throws IOException {
 
         return ApiResponse.success(SuccessType.PUSH_ALARM_SUCCESS, fcmService.pushAlarm(request, JwtProvider.getUserFromPrincial(principal)));
-    }
-
-    /**
-     * 주기적 알림 전송 테스트를 위한 API
-     */
-    @PostMapping("/qna")
-    @ResponseStatus(HttpStatus.OK)
-    public ApiResponse sendTopicScheduledTest() {
-        return ApiResponse.success(SuccessType.PUSH_ALARM_SUCCESS, fcmScheduler.pushTodayQna());
     }
 
     @PostMapping("/parentchild")
