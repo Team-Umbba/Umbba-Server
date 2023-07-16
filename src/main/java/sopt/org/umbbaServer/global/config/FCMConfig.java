eb54afb0c24c8d0b30787ef98e1f0802b4f9fd33
jun02160 <jun020216@naver.com>
2023-07-17 02:34:42 +0900

    [CHORE] application.yml ddl-auto 변경

--- src/main/java/sopt/org/umbbaServer/global/config/FCMConfig.java
+++ src/main/java/sopt/org/umbbaServer/global/config/FCMConfig.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
 import sopt.org.umbbaServer.global.exception.CustomException;
 import sopt.org.umbbaServer.global.exception.ErrorType;
 import sopt.org.umbbaServer.global.util.fcm.controller.dto.FCMPushRequestDto;
