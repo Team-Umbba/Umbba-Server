f2495ead38073adcf0323959a7d879f2b5164ba5
ddongseop <dlehdtjq00o@gmail.com>
2023-08-12 17:50:18 +0900

    [FIX] EntityManager close 추가 #88

--- umbba-notification/src/main/java/sopt/org/umbba/notification/service/fcm/FCMService.java
+++ umbba-notification/src/main/java/sopt/org/umbba/notification/service/fcm/FCMService.java
@@ -233,6 +233,7 @@ public class FCMService {
                         log.info("스케줄링 작업 예약 내 addCount 후 count: {}", pc.getCount());
 
                         QnA todayQnA = parentchild.getQnaList().get(parentchild.getCount() - 1);
+                        em.close();
 
                         log.info("\n  Current QnA: {}  \n  Today QnA: {}", currentQnA.getId(), todayQnA.getId());
                         if (todayQnA == null) {
