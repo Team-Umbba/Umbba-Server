577fca843b8c893dd7e4177e68c0c04476936e43
ddongseop <dlehdtjq00o@gmail.com>
2023-08-14 23:42:28 +0900

    [CHORE} CD시에 프로세스가 죽을 때까지 반복문으로 대기하도록 변경 #95

--- scripts/umbba-notification/deploy.sh
+++ scripts/umbba-notification/deploy.sh
@@ -7,6 +7,10 @@ TARGET_PID=$(lsof -Fp -i TCP:${TARGET_PORT} | grep -Po 'p[0-9]+' | grep -Po '[0-
 if [ ! -z ${TARGET_PID} ]; then
   echo "[$NOW_TIME] Kill WAS running at ${TARGET_PORT}." >> /home/ubuntu/notification-server/deploy.log
   sudo kill -15 ${TARGET_PID}
+  while ps -p $TARGET_PID > /dev/null; do
+      sleep 1
+    done
+  echo "[$NOW_TIME] 애플리케이션이 정상 종료되었습니다."
 fi
 
 nohup java -jar -Duser.timezone=Asia/Seoul -Dspring.profiles.active=notification $BUILD_PATH >> /home/ubuntu/notification-server/deploy.log 2>/home/ubuntu/notification-server/deploy_err.log &
