5a46c9fec8275ab425be1b3ec9515da83fab19a8
ddongseop <dlehdtjq00o@gmail.com>
2023-08-14 21:22:37 +0900

    [FIX] MessageDto 관련 어노테이션 정리 #93

--- umbba-common/src/main/java/sopt/org/umbba/common/sqs/dto/ScheduleDto.java
+++ umbba-common/src/main/java/sopt/org/umbba/common/sqs/dto/ScheduleDto.java
@@ -3,7 +3,6 @@ package sopt.org.umbba.common.sqs.dto;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 import lombok.extern.slf4j.Slf4j;
 import sopt.org.umbba.common.sqs.MessageType;
@@ -12,7 +11,6 @@ import sopt.org.umbba.common.sqs.MessageType;
 @Getter
 @SuperBuilder
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class ScheduleDto extends MessageDto{
 
     public static ScheduleDto of() {
