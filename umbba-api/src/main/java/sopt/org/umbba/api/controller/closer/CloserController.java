b63e15cd006f24329e56a848e9329eb12b5742fe
ddongseop <dlehdtjq00o@gmail.com>
2024-03-07 15:00:38 +0900

    [FEAT] 가까워지기 질문에 답변하기 API 구현 #136

--- umbba-api/src/main/java/sopt/org/umbba/api/controller/closer/CloserController.java
+++ umbba-api/src/main/java/sopt/org/umbba/api/controller/closer/CloserController.java
@@ -3,16 +3,18 @@ package sopt.org.umbba.api.controller.closer;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import sopt.org.umbba.api.config.jwt.JwtProvider;
+import sopt.org.umbba.api.controller.closer.dto.request.TodayCloserAnswerRequestDto;
 import sopt.org.umbba.api.controller.closer.dto.response.TodayCloserQnAResponseDto;
 import sopt.org.umbba.api.service.closer.CloserService;
 import sopt.org.umbba.common.exception.SuccessType;
 import sopt.org.umbba.common.exception.dto.ApiResponse;
 
+import javax.validation.Valid;
 import java.security.Principal;
+
+import static sopt.org.umbba.common.exception.SuccessType.ANSWER_TODAY_CLOSER_QUESTION_SUCCESS;
 
 @Slf4j
 @RestController("/closer")
@@ -21,9 +23,16 @@ public class CloserController {
 
     private final CloserService closerService;
 
-    @PatchMapping("/today")
+    @GetMapping("/today")
     @ResponseStatus(HttpStatus.OK)
-    public ApiResponse<TodayCloserQnAResponseDto> inviteRelation(Principal principal) {
+    public ApiResponse<TodayCloserQnAResponseDto> getTodayCloserQnA(Principal principal) {
         return ApiResponse.success(SuccessType.GET_TODAY_CLOSER_QNA_SUCCESS, closerService.getTodayCloserQnA(JwtProvider.getUserFromPrincial(principal)));
+    }
+
+    @PatchMapping("/answer")
+    @ResponseStatus(HttpStatus.OK)
+    public ApiResponse<?> answerTodayCloserQnA(Principal principal, @Valid @RequestBody final TodayCloserAnswerRequestDto request) {
+        closerService.answerTodayCloserQnA(JwtProvider.getUserFromPrincial(principal), request);
+        return ApiResponse.success(ANSWER_TODAY_CLOSER_QUESTION_SUCCESS);
     }
 }
--- umbba-api/src/main/java/sopt/org/umbba/api/service/closer/CloserService.java
+++ umbba-api/src/main/java/sopt/org/umbba/api/service/closer/CloserService.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import sopt.org.umbba.api.controller.closer.dto.request.TodayCloserAnswerRequestDto;
 import sopt.org.umbba.api.controller.closer.dto.response.TodayCloserQnAResponseDto;
 import sopt.org.umbba.common.exception.ErrorType;
 import sopt.org.umbba.common.exception.model.CustomException;
@@ -20,7 +21,6 @@ import sopt.org.umbba.domain.domain.user.repository.UserRepository;
 public class CloserService {
 
     private final UserRepository userRepository;
-    private final CloserQnARepository closerQnARepository;
 
     public TodayCloserQnAResponseDto getTodayCloserQnA(Long userId) {
         User user = getUserById(userId);
@@ -52,6 +52,27 @@ public class CloserService {
             } else { // Case 3,4 (둘다 답변한 경우)
                 return TodayCloserQnAResponseDto.of(todayQnA, 3, false);
             }
+        }
+    }
+
+    @Transactional
+    public void answerTodayCloserQnA(Long userId, TodayCloserAnswerRequestDto request) {
+        User user = getUserById(userId);
+        Parentchild parentchild = user.getParentChild();
+        if (parentchild == null) {
+            throw new CustomException(ErrorType.USER_HAVE_NO_PARENTCHILD);
+        }
+
+        if (user.isMeChild()) {
+            int closerCount = parentchild.getCloserChildCount();
+            CloserQnA todayQnA = parentchild.getCloserQnaList().get(closerCount);
+
+            todayQnA.saveChildAnswer(request.getAnswer());
+        } else {
+            int closerCount = parentchild.getCloserParentCount();
+            CloserQnA todayQnA = parentchild.getCloserQnaList().get(closerCount);
+
+            todayQnA.saveParentAnswer(request.getAnswer());
         }
     }
 
